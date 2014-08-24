1d2e07f11cc86e913c624ce8133f1364fe409d66
Bernardo Heynemann <heynemann@gmail.com>
2014-08-23 21:32:22 -0300

    fish bundles root path variable

--- config.fish
+++ config.fish
@@ -2,17 +2,17 @@ if test "$VIRTUALFISH_ENABLE_COMPAT_ALIASES" != "0"
     set -g VIRTUALFISH_COMPAT_ALIASES
 end
 
-source $bundle_root_path/functions/virtual.fish
+source $__fish_bundles_bundle_root_path/functions/virtual.fish
 
 # optional plugins
 if test "$VIRTUALFISH_ENABLE_AUTO_ACTIVATION" != "0"
-    source $bundle_root_path/functions/auto_activation.fish
+    source $__fish_bundles_bundle_root_path/functions/auto_activation.fish
 end
 
 if test "$VIRTUALFISH_ENABLE_GLOBAL_REQUIREMENTS" != "0"
-    source $bundle_root_path/functions/global_requirements.fish
+    source $__fish_bundles_bundle_root_path/functions/global_requirements.fish
 end
 
 if test "$VIRTUALFISH_ENABLE_PROJECTS" != "0"
-    source $bundle_root_path/functions/projects.fish
+    source $__fish_bundles_bundle_root_path/functions/projects.fish
 end
